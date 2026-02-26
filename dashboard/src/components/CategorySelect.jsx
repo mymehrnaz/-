7bd5b5a2ec444e722e1a4d3e21848c5cca6da634
mymehrnaz <165458618+mymehrnaz@users.noreply.github.com>
2026-02-26 14:51:13 +0330

    remove some lines

--- dashboard/src/components/CategorySelect.jsx
+++ dashboard/src/components/CategorySelect.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { Listbox } from "@headlessui/react";
-
 export default function CategorySelect({ categories, selected, onChange }) {
-  return (
-    <Listbox value={selected} onChange={onChange} multiple>
-      <div className="relative w-64">
-        <Listbox.Button className="border rounded p-2 w-full text-left">
-          {selected.length ? selected.join(", ") : "Select categories"}
-        </Listbox.Button>
+  function handleChange(e) {
+    const value = Array.from(e.target.selectedOptions, o => o.value);
+    onChange(value);
+  }
 
-        <Listbox.Options className="absolute mt-1 w-full bg-white border rounded shadow z-10">
-          {categories.map(cat => (
-            <Listbox.Option key={cat} value={cat}>
-              {cat}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </div>
-    </Listbox>
+  return (
+    <select
+      multiple
+      value={selected}
+      onChange={handleChange}
+      className="border rounded p-2 w-64"
+    >
+      {categories.map(cat => (
+        <option key={cat} value={cat}>
+          {cat}
+        </option>
+      ))}
+    </select>
   );
 }
+
